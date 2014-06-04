5ecec15676336bdb3ac697c618e458f5c5c415a7
Jeffrey Hung <hwdhung@gmail.com>
2014-06-05 03:20:10 +0800

    the initial function done

--- Thread_Server.java
+++ Thread_Server.java
@@ -1,93 +1,106 @@
-
-// 加入IO及net
 import java.io.*;
 import java.net.*;
 import java.util.HashMap;
 import java.util.Map;
 
-// Server端
+// Server
 public class Thread_Server
 {
-	// 程式進入點
+	// program entry
 	public static void main(String[] argv) throws Exception
 	{
-		// 針對使用者的id編號(從0開始)
+		// ID map to index(start from 0)
 		int now, id = 0;
-		String username[20];
-		// 建立Map-Hashing物件
+		
+		String parseCom[] = new String[10];
+		// construct Map-Hashing Object
 		Map<String, Integer> map = new HashMap<String, Integer>();
 
-		// 儲存Input Thread的buffer，最大上限20人
+		// the buffer for store Input Thread, 20 people max
 		String[] buf = new String[20];
 		for(int i = 0; i < 20 ; ++i) buf[i]="";
-		
-		// 建立ServerSocket物件
+		String username[] = new String[20];
+		// ServerSocket Object
 		ServerSocket serverSocket = new ServerSocket(45321);
 		
-		// 不斷取得使用者連線
+		
 		while(true)
 		{
-			// 建立Client端連線
+			// Client connection
 			Socket connectionSocket = serverSocket.accept();
 			
-			// 從Client端取得Buffer
+			// get buffer from client
 			BufferedReader inFromClient = 
 				new BufferedReader(new InputStreamReader(connectionSocket.getInputStream())); 
 
-			// 先取得使用者的ID
+			// first get user's ID
 			String user = inFromClient.readLine();
 			
-			// 取得Client端的String
-			String clientString = inFromClient.readLine();
-			
-			// 如果還沒有建立使用者ID
+			// the user is first time log in
 			if(!map.containsKey(user))
 			{
-				// 建立使用者訊息
+				// the message of construct user
 				System.out.println("Create user: "+user+"->"+id);
 				
-				// 建立使用者ID的MAP
+				//put the ID into string array
+				username[id] = user;
+				
+				// build the map of user ID and index
 				map.put(user, id++);
 			}
 			
-			// 取得使用者ID
+			// get user Index
 			now = map.get(user).intValue();
 			
-			// 如果是Input Thread
+			System.out.println("the user: " + username[now] + "is logging.");// test
+			
+			///////////////////////////////////////
+			DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+						
+			if(!buf[now].equals(""))
+				outToClient.writeBytes(buf[now]);
+			else
+				outToClient.writeBytes("none");
+			//////////////////////////////////////
+			
+			// second get user's action
+			String clientString = inFromClient.readLine();
+			
+			parseCom = clientString.split("#");
 			if(clientString.charAt(0)=='I')
 			{
-				// 儲存到其他使用者的buffer
+				
 				for(int i = 0; i < 20 ; ++i)
 				{
-					// 除了自己以外的所有使用者
+				
 					if(i != now)
 						buf[i] = buf[i].concat(user+" says: "+clientString.substring(1,clientString.length())+"#");
 				}
 				
-				// 輸出到Server端的畫面
+			
 				System.out.println("Client("+connectionSocket.getInetAddress()+"): " + clientString);
 			}
-			// 如果是Request Thread
+			// if Request Thread
 			else
 			{
-				// 建立輸出至Client端的Stream
-				DataOutputStream  outToClient = 
-						new DataOutputStream(connectionSocket.getOutputStream());
+				// The stream output to client
+				//DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+						
 				
-				// 回覆至Client端的String
+				// the string reply to client
 				String reply;
 				
-				// 如果buffer有東西
+				// if the buffer not empty
 				if(!buf[now].equals("")) reply = "Y";
 				else reply = "N";
 				
-				// 回覆至Client端的String
+				// the string reply to client
 				reply = reply.concat(buf[now]);
 				
-				// 回覆至Client端
+				// reply to client
 				outToClient.writeBytes(reply+'\n');
 				
-				// 清空buffer
+				// erase buffer
 				buf[now] = "";
 			}
 			
